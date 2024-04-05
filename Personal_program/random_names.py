e906c0d9bcc0f568ede5932c5a4228cffc585fc9
James Brady <bra16046@byui.edu>
2024-04-04 20:59:19 -0600

    updated python final project everything works, just need to make unit tests now.

--- Personal_program/random_names.py
+++ Personal_program/random_names.py
@@ -1,23 +1,53 @@
 from tkinter import *
-from random import *
-from pytest import *
+import random
+import pytest
+import csv
 
-root = Tk()
-root.geometry('300x300')
 
-l = Label(root, text="Hello World!")
-l.pack()
 
-b = Button(root, text="Click me!")
-b.pack()
-
-root.mainloop()
+csv_file_names = "C:\\Users\\cjcri\\OneDrive\\Documents\\BYU_I\\cse111\\Personal_program\\names.csv"
+csv_file_expressions =  "C:\\Users\\cjcri\\OneDrive\\Documents\\BYU_I\\cse111\\Personal_program\\expressions.csv"
 
 
 # def main():
 
-# def student_name():
+# def student_name(csv_file):
 #     with open Names.CSV as names:
+def student_name(csv_file_names):
+    with open(csv_file_names) as f:
+        reader = csv.reader(f)
+        chosen_row = random.choice(list(reader))
+        return chosen_row[0]
+
+def expression(csv_file_expressions):
+     with open(csv_file_expressions) as f:
+        reader = csv.reader(f)
+        chosen_row = random.choice(list(reader))
+        return chosen_row[0]
+
+def on_button_click():
+    random_name_perclick = student_name(csv_file_names)
+    random_expression_perclick = expression(csv_file_expressions)
+    result_label.config(text=f"{random_name_perclick} what is the value of {random_expression_perclick}?")
+
+
+
+root = Tk()
+root.geometry('300x300')
+
+l = Label(root, text="Today's Hero is!")
+l.pack()
+
+result_label = Label(root, text="")
+result_label.pack()
+
+button = Button(root, text="Click Me", command=on_button_click)
+button.pack()
+
+root.mainloop()
+
+
+
 ## Open Names.CSV 
     ##get a random name
     ##print name to GUI
--- Personal_program/test_random_names.py
+++ Personal_program/test_random_names.py
@@ -1,12 +1,26 @@
 import pytest
 import random_names
 
-test_student_name():
-""" Verify that student_name is generating a random name from the Names.CSV print to GUI 
-    Paramaters:
-    Return: """"
 
-test_expression():
-""" Verify that student_name is generating a random exprestion from the Expression.CSV print to GUI
-    Paramaters:
-    Return: """"
+def test_student_name():
+    """ Verify that student_name is generating a random name from the Names.CSV print to GUI 
+    Paramaters:csv_file_names
+    Return: return chosen_row[0]
+    """"
+    names_1 = random_names(csv_file_names)
+    names_2 = random_names(csv_file_names)
+    assert names_1 =
+
+
+
+
+def test_expression():
+    """ Verify that student_name is generating a random exprestion from the Expression.CSV print to GUI
+    Paramaters:csv_file_expressions
+    Return: return chosen_row[0]
+    """"
+
+def test_on_button_click():
+    """Verify that on_button_click generates the returned random rows of functions expression and student_name.
+    Paramaters: None
+    Return: student_name chosen_row[0],  expression return chosen_row[0]"""
